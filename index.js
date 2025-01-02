0b939de45f1de469013ab20652ad76d13ae61790
Suraj Gupta <8700suraj@gmail.com>
2025-01-02 15:12:22 +0530

    fs1

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const connectDB = async () =>{
+    mongoose.connect("mongodb+srv://8700suraj:21hJIFETV1vdRFDL@testt-db.5igjwzu.mongodb.net/e-commerse");
+    const productSchema = new mongoose.Schema({});
+    // const product = mongoose.model('product', productSchema);
+    // const data = await product.find();
+    const users = mongoose.model
 
-app.get('/', (req,resp)=> {
-    resp.send("backend is working...");
+    console.warn(data);
+}
+connectDB();
 
-
-});
 
 app.listen(5000);
