e0b32c3ebd1d1a25a8576c76fe8c00926117f767
Suraj Gupta <8700suraj@gmail.com>
2025-01-21 19:58:48 +0530

    15D

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ const User = require('./db/User');
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 app.post("/register", async (req, res)=>{
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password;
     res.send(result)
 })
+
+app.post("/login", async (req, resp) => {
+    if(req.body.password && req.body.email){
+       let user = await User.findOne(req.body).select("-password");
+       if (user) {
+          resp.send(user)
+       } else {
+          resp.send({result: 'No user found'});
+       }     
+   } else {
+    resp.send({result: 'No user found'});
+   }
+    
+   
+   })
 
 app.listen(5000);
