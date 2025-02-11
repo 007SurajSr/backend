e3ac94bbcff3970151a31e276d659bcdf1ed69bc
Suraj Gupta <8700suraj@gmail.com>
2025-02-11 19:22:32 +0530

    32D

--- index.js
+++ index.js
@@ -66,4 +66,15 @@ app.get('/update/:id', async (req,resp) =>{
    res.send(result);
 });
 
+app.get('/search/:key', async (req,resp)=>{
+   let result = await Product.find({
+      "$or": [
+         {name : {$regex: req.params.key}},
+         {company : {$regex: req.params.key}},
+         {price : {$regex: req.params.key}},
+         {category : {$regex: req.params.key}}
+      ]
+   });
+   resp.send(result);
+})
 app.listen(5000);
