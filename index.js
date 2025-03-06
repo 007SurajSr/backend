af152a2d75c61adee6cfb914d5e80e6207726db2
Suraj Gupta <8700suraj@gmail.com>
2025-03-06 19:22:12 +0530

    35D

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.post("/register", async (req, res)=>{
+app.post("/register",   async (req, res)=>{
     let user = new User(req.body);
     let result = await user.save();
     result = result.toObject();
@@ -25,7 +25,7 @@ app.post("/register", async (req, res)=>{
    })
 })
 
-app.post("/login", async (req, resp) => {
+app.post("/login", verifyToken, async (req, resp) => {
     if(req.body.password && req.body.email){
        let user = await User.findOne(req.body).select("-password");
        if (user) {
@@ -43,13 +43,13 @@ app.post("/login", async (req, resp) => {
     resp.send({result: 'No user found'});
    }})
 
- app.post("/add-product", async (req,resp) => {
+ app.post("/add-product", verifyToken, async (req,resp) => {
    let product = new Product(req.body);
    let result = await product.save();
    resp.send(result);
  })
 
- app.get('/products', async (req,resp) =>{
+ app.get('/products', verifyToken, async (req,resp) =>{
    let product = await Product.find();
    if(product.length>0){
       resp.send(product)
@@ -58,13 +58,13 @@ app.post("/login", async (req, resp) => {
    }
  });
 
- app.delete('/delete/:id', async (req, resp) =>{
+ app.delete('/delete/:id', verifyToken, async (req, resp) =>{
      
     const result = await Product.deleteOne({_id:req.params.id});
     resp.send(result);
  });
 
-app.get('/update/:id', async (req,resp) =>{
+app.get('/update/:id', verifyToken, async (req,resp) =>{
    let result = await Product.findOne({_id: req.params.id});
    if (result){
       resp.send(result)
@@ -73,7 +73,7 @@ app.get('/update/:id', async (req,resp) =>{
    }
 });
 
- app.put('/product/:id', async (req,res) =>{
+ app.put('/product/:id', verifyToken, async (req,res) =>{
    let result = await Product.updateOne(
     {_id: req.params.id},
     {
@@ -82,7 +82,7 @@ app.get('/update/:id', async (req,resp) =>{
    res.send(result);
 });
 
-app.get('/search/:key', async (req,resp)=>{
+app.get('/search/:key', verifyToken, async (req,resp)=>{
    let result = await Product.find({
       "$or": [
          {name : {$regex: req.params.key}},
@@ -93,5 +93,22 @@ app.get('/search/:key', async (req,resp)=>{
    });
    resp.send(result);
 })
+
+function verifyToken(req,resp,next){
+   let token = req.headers['authorization'];
+   if(token){
+      token = token.split( ' ')[1];
+      Jwt.verify(token, jwtKey, (err, valid) => {
+         if(err) 
+            {
+             resp.status(401).send({result: "Please provide valid token "});
+         } else {
+            next();
+         }
+      })
+   } else {
+      resp.status(403).send({ result : " Please add token with header"});
+   }
+}
 
 app.listen(5000);
